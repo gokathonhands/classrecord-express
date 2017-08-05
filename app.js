b5a2fd256f52f9d6f73e4d08c864d5ee598f0c59
pjo901018 <pjo901018@korea.ac.kr>
2017-08-05 16:22:57 +0900

    firebase dbhelper createCourse까지 추가

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const app = express();
-const firebaseinit = require('./firebase/firebaseinit');
+const firebaseinit = require('./firebase/init/firebaseinit');
+const fbdbhelper = require('./firebase/database/fbdbhelper');
 
 firebaseinit();
 
 app.get('/', (req, res) => {
-			  res.send('Hello World!')
-				})
+	fbdbhelper("123", "key",]"title");
+
+    res.send('Hello World!')
+})
 
 // app.use('/api',require('./routes/api'))
 
 app.listen(3000, function () {
-		  console.log('Example app listening on port 3000!')
-			})
+    console.log('Example app listening on port 3000!')
+})
