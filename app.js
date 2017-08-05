d8c7f9adef64fac8870e3dabbc341532194679ce
Joel <retbird13@gmail.com>
2017-08-05 16:19:55 +0900

    Revert "upload routing"
    This reverts commit 2093eb69d16d6271c8ff4a557ed7b3adf47a208a.

--- app.js
+++ app.js
@@ -1,24 +1,12 @@
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
+
 app.get('/', (req, res) => {
 			  res.send('Hello World!')
 				})
 
-//app.use('/api',require('./routes/api'))
-app.post('/upload',function(req,res){
-		fs.readFile(req.files.uploadFile.path,function(error,data){
-				var filePath=__dirname+"\\files\\"+req.files.upladFile.name
-				fs.writeFile(filePath,data,function(error){
-						if(error){
-							throw err;
-						}
-						else {
-							res.redirect("back");
-						}
-						})
-				})
-	})
+app.use('/api',require('./routes/api'))
+
 app.listen(3000, function () {
 		  console.log('Example app listening on port 3000!')
 			})
